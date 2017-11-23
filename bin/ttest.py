cc2409e3e14fce4d58a792bee5e251ae88c44662
Alexander Harvey Nitz <alex.nitz@ligo.org>
2017-11-23 03:13:45 +0100

    update script

--- bin/ttest.py
+++ bin/ttest.py
@@ -2,52 +2,60 @@ from keras.layers import *
 from keras.models import *
 import pycbc.psd
 import gmind.generator
+import matplotlib
+matplotlib.use('Agg')
 
 p = pycbc.psd.aLIGOZeroDetHighPower(2**19, 1.0/16, 15)
 s = gmind.generator.GaussianNoiseGenerator(32, 1024, p, 20)
 p = gmind.generator.WFParamGenerator(["examples/test.ini"])
-g = w = gmind.generator.GaussianSignalTimeGenerator(s, p, [-3, 1], batch=100)
+g = w = gmind.generator.GaussianSignalTimeGenerator(s, p, [-3, 1], batch=50)
 
 # dimensions of our images.
 width = 1024 * 4
  
 model = Sequential()
 model.add(Conv1D(32, 3, input_shape=(width, 1), padding='causal'))
-model.add(BatchNormalization())
 
 for i in range(6):
     model.add(Conv1D(32, 8, padding='causal'))
-    model.add(BatchNormalization())
 
-for i in range(4):
+for i in range(6):
     model.add(Conv1D(32, 3, padding='causal'))
-    model.add(BatchNormalization())
     model.add(MaxPooling1D(2))
 
 model.add(Flatten())
 model.add(Dense(128))
-model.add(Activation("linear"))
 model.add(Dense(16))
 model.add(Dense(1))
 
-test = False
-retrain = False
-if test or retrain:
+retrain = True
+if retrain:
     model.load_weights('test.hdf')
 
 model.compile(loss='mean_squared_error',
-              optimizer='adam',
+              optimizer='nadam',
               metrics=['accuracy'])
 
-if test:
+i = 0
+while 1:
+    #FIT ME
+    d, t = g.next()
+    model.fit(d, t, batch_size=g.batch, epochs=100)
+    model.save('test.hdf')
+    
+    #TESTS I UNDERSTAND
     s, t = g.next()
     p = model.predict(s, 1)
+    
+    ms = (t - p)**2.0
+    print i, ms.mean(), ms.max(), p.min(), p.max()
+    
     import pylab
-    pylab.figure(1)
+    pylab.figure()
     pylab.scatter(t, p)
-    pylab.show()
-
-if not test:
-    model.fit_generator(g, 20, epochs=1)
-    model.save('test.hdf')
+    pylab.xlim(4, 20)
+    pylab.ylim(4, 20)
+    pylab.savefig('iter-%s.png' % i)
+    pylab.close('all')
+    i += 1
 
